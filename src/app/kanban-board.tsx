68f54a2f1044b8fdfac687cfcd6f3a123819b3d8
Petros <93052465+pkitazos@users.noreply.github.com>
2024-04-29 15:33:00 +0100

    made columns sortable

--- src/app/kanban-board.tsx
+++ src/app/kanban-board.tsx
@@ -2,8 +2,19 @@
 
 import { ColumnContainer } from "@/components/column-container";
 import { Button } from "@/components/ui/button";
+import {
+  DndContext,
+  DragOverlay,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  type DragEndEvent,
+  type DragStartEvent,
+} from "@dnd-kit/core";
+import { SortableContext, arrayMove } from "@dnd-kit/sortable";
 import { Plus } from "lucide-react";
-import { useState } from "react";
+import { useMemo, useState } from "react";
+import { createPortal } from "react-dom";
 
 export type Id = string | number;
 
@@ -20,29 +31,57 @@ type Task = {
 
 export function KanbanBoard() {
   const [columns, setColumns] = useState<Column[]>([]);
+  const columnIds = useMemo(() => columns.map((col) => col.id), [columns]);
 
-  console.log(columns);
+  const [activeColumn, setActiveColum] = useState<Column | null>();
+
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 10, // 10 px
+      },
+    }),
+  );
 
   return (
     <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-10">
-      <div className="m-auto flex gap-4">
-        <div className="flex gap-4">
-          {columns.map((col) => (
-            <ColumnContainer
-              key={col.id}
-              column={col}
-              deleteColumn={deleteColumn}
-            />
-          ))}
+      <DndContext
+        sensors={sensors}
+        onDragStart={onDragStart}
+        onDragEnd={onDragEnd}
+      >
+        <div className="m-auto flex gap-4">
+          <div className="flex gap-4">
+            <SortableContext items={columnIds}>
+              {columns.map((col) => (
+                <ColumnContainer
+                  key={col.id}
+                  column={col}
+                  deleteColumn={deleteColumn}
+                />
+              ))}
+            </SortableContext>
+          </div>
+          <Button
+            className="flex items-center gap-2"
+            size="lg"
+            onClick={createNewColumn}
+          >
+            <Plus /> Add Column
+          </Button>
         </div>
-        <Button
-          className="flex items-center gap-2"
-          size="lg"
-          onClick={createNewColumn}
-        >
-          <Plus /> Add Column
-        </Button>
-      </div>
+        {createPortal(
+          <DragOverlay>
+            {activeColumn && (
+              <ColumnContainer
+                column={activeColumn}
+                deleteColumn={deleteColumn}
+              />
+            )}
+          </DragOverlay>,
+          document.body,
+        )}
+      </DndContext>
     </div>
   );
 
@@ -58,8 +97,38 @@ export function KanbanBoard() {
   function deleteColumn(id: Id) {
     setColumns((prev) => prev.filter((s) => s.id !== id));
   }
+
+  function onDragStart({ active }: DragStartEvent) {
+    console.log("DRAG START");
+    if (active.data.current?.type === "Column") {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      setActiveColum(active.data.current.column);
+      return;
+    }
+  }
+
+  function onDragEnd({ active, over }: DragEndEvent) {
+    if (!over) return;
+
+    const activeColId = active.id;
+    const overColId = over.id;
+
+    if (activeColId === overColId) return;
+
+    setColumns((columns) => {
+      const activeColIdx = findColIndex(columns, activeColId);
+      const overColIdx = findColIndex(columns, overColId);
+
+      return arrayMove(columns, activeColIdx, overColIdx);
+    });
+  }
 }
 
 function generateId() {
+  // get actual unique id
   return Math.floor(Math.random() * 10001);
+}
+
+function findColIndex(columns: Column[], id: Id) {
+  return columns.findIndex((col) => col.id === id);
 }
--- src/components/column-container.tsx
+++ src/components/column-container.tsx
@@ -8,17 +8,58 @@ import {
   CardHeader,
   CardTitle,
 } from "./ui/card";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 export function ColumnContainer({
-  column: { id, title, tasks },
+  column,
   deleteColumn,
 }: {
   column: Column;
   deleteColumn: (id: Id) => void;
 }) {
+  const { id, title, tasks } = column;
+
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id,
+    data: {
+      type: "Column",
+      column,
+    },
+  });
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
+
+  if (isDragging)
+    return (
+      <Card
+        ref={setNodeRef}
+        style={style}
+        className="h-[500px] max-h-[500px] w-[350px] border-2 border-accent bg-slate-900 opacity-50"
+      />
+    );
+
   return (
-    <Card className="h-[500px] max-h-[500px] w-[350px] bg-slate-900">
-      <CardHeader className="flex-row items-center justify-between rounded-lg border-4 border-slate-900 bg-slate-950 p-3">
+    <Card
+      ref={setNodeRef}
+      style={style}
+      className="h-[500px] max-h-[500px] w-[350px] bg-slate-900"
+    >
+      <CardHeader
+        className="flex-row items-center justify-between rounded-lg border-4 border-slate-900 bg-slate-950 p-3"
+        {...attributes}
+        {...listeners}
+      >
         <div className="flex items-center gap-2">
           <div className="rounded-full bg-slate-900 px-3.5 py-2">
             {tasks.length}
@@ -29,8 +70,8 @@ export function ColumnContainer({
           <X />
         </Button>
       </CardHeader>
-      <CardContent>{}</CardContent>
-      <CardFooter></CardFooter>
+      <CardContent className="flex-grow">content</CardContent>
+      <CardFooter>footer</CardFooter>
     </Card>
   );
 }
